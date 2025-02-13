4978ba1983473c52430be0e716cd4f9274ac023a
aron7 <aron@beeldr.nl>
2025-02-13 12:19:58 +0100

    first commit

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function welcome(){
+    let welcomeDiv = document.getElementById("welcome");
+    let page = document.getElementById("page");
+
+    welcomeDiv.classList.add("menu-state")
+    page.classList.add("page-state")
+}
